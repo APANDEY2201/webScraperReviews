aa89550cf6944fc7194fbf5dba9208ca1ea95264
Jaykishan <jkbaldania@gmail.com>
2020-05-10 22:29:29 +0200

    Exploratory analysis 1

--- venv/Scripts/DataAnalysis.py
+++ venv/Scripts/DataAnalysis.py
@@ -1,6 +1,11 @@
 import numpy as np
 import csv
 import pandas as pd
+from sklearn.cluster import KMeans
+
+# ----------------------------------------------------------------------------
+# Data imports
+# ----------------------------------------------------------------------------
 
 csvFileName1 = 'Master_Data_Milestone1_Fitted.csv' #Master_Data_Milestone1_Small_for_training.csv'
 analysisDataSource = list(csv.reader(open(csvFileName1, encoding='utf-8'), delimiter='|'))  # CSV file to 2 dimensional list of string
@@ -10,6 +15,10 @@ analysisOrgData = list(csv.reader(open(csvFileName2, encoding='utf-8'), delimite
 
 dataFrame = pd.DataFrame(analysisDataSource[1:],columns=analysisDataSource[0])
 dataFrameOrg = pd.DataFrame(analysisOrgData[1:],columns=analysisOrgData[0])
+
+# ----------------------------------------------------------------------------
+# Data formatting
+# ----------------------------------------------------------------------------
 
 dataFrame['RvScore'] = dataFrame['RvScore'].replace(to_replace ="NA",value ="-10").replace(to_replace ="  ",value ="-10")
 dataFrame['RvScore'] = dataFrame['RvScore'].astype(float)
@@ -26,6 +35,10 @@ dataFrame['topic9'] = dataFrame['topic9'].astype(float)
 
 dataFrame['RvScore'] = dataFrame['RvScore'] - 2.5
 dataFrame['RvScore2'] = np.where(dataFrame['RvScore'] < 0, "Neg", "Pos")
+
+# ----------------------------------------------------------------------------
+# Checking averaged topics in + and - sentiments
+# ----------------------------------------------------------------------------
 
 # print(dataFrame)
 #
@@ -52,5 +65,22 @@ negDistribution = negDistribution / cntNeg
 # print(posDistribution)
 # print(negDistribution)
 
+# ----------------------------------------------------------------------------
+# Clustering
+# ----------------------------------------------------------------------------
+
+noOfClusters = 5
+
 dataFramePos = dataFrame[dataFrame['RvScore2']=="Pos"]
 dataFrameNeg = dataFrame[dataFrame['RvScore2']=="Neg"]
+
+dataFrame_KMeans = dataFrame.iloc[:,10:20]
+dataFramePos_KMeans = dataFramePos.iloc[:,10:20]
+dataFrameNeg_KMeans = dataFrameNeg.iloc[:,10:20]
+
+# print(dataFrame_KMeans.head(20))
+
+kmeans_df = KMeans(n_clusters=noOfClusters, random_state=0).fit(dataFrame_KMeans)
+dataFrame['Cluster'] = kmeans_df.labels_
+
+print(dataFrame)
